9ba496df4477eb9c8593e3a28fbe0b0ae262d81d
nayan <nayansharma1010@gmail.com>
2023-02-28 10:37:03 +0530

    feat: optimising with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config,&'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
-
+    pub fn build(mut args : impl Iterator<Item = String>) -> Result<Config,&'static str>{
+        args.next();
+        
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        return Ok(Config { query,file_path,ignore_case});
+        return Ok(Config { query, file_path, ignore_case})
+    
     }
 }
 
@@ -30,13 +35,7 @@ pub fn run(config:Config) -> Result<(),Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents : &'a str) -> Vec<&'a str> {
-    let mut results:Vec<&str> = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    return results;
+    return contents.lines().filter(|line| line.contains(query)).collect();
 }
 
 pub fn search_case_insensitive<'a>(query:&str,contents : &'a str) -> Vec<&'a str> {
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use std::process;
 use mini_grep::Config;
 
 fn main(){
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments:{err}");
         process::exit(1);
     });
